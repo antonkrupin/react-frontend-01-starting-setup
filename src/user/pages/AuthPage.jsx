ee02c3b75e924bd9bb27b5353c3e4322321be343
Anton Krupin <krupin_anton@mail.ru>
2023-03-14 20:33:56 +0300

    working with backend

--- src/user/pages/AuthPage.jsx
+++ src/user/pages/AuthPage.jsx
@@ -3,6 +3,8 @@ import React, { useState, useContext } from 'react';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import Card from '../../shared/components/UIElements/Card';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 import {
   VALIDATOR_EMAIL,
@@ -21,6 +23,10 @@ const AuthPage = () => {
 
   const [isLogginMode, setLoggingMode] = useState(true);
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [error, setError] = useState(null);
+
   const [formState, inputHandler, setFormData] = useForm({
     email: {
       value: '',
@@ -34,8 +40,30 @@ const AuthPage = () => {
 
   const authFormSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     if (isLogginMode) {
+      try {
+        const response = await fetch('http://localhost:5000/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          })
+        });
 
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || 'Something went wrong. Please try again.');
+      }
     } else {
       try {
         const response = await fetch('http://localhost:5000/api/users/signup', {
@@ -51,13 +79,16 @@ const AuthPage = () => {
         });
 
         const data = await response.json();
-        console.log(data);
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+        setIsLoading(false);
+        auth.login();
       } catch (err) {
-        console.log(err);
+        setIsLoading(false);
+        setError(err.message || 'Something went wrong. Please try again.');
       }
     }
-    
-    auth.login();
   };
 
   const switchLoggingMode = () => {
@@ -82,46 +113,54 @@ const AuthPage = () => {
     setLoggingMode(prevMode => !prevMode);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card className="authentication">
-      <form onSubmit={authFormSubmit}>
-        {!isLogginMode && (
+    <>
+      <ErrorModal error={error} onClear={errorHandler} />
+      <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay />}
+        <form onSubmit={authFormSubmit}>
+          {!isLogginMode && (
+            <Input
+            id="name"
+            element="input"
+            type="text"
+            label="Name"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a Name."
+            onInput={inputHandler}
+          />
+          )}
           <Input
-          id="name"
-          element="input"
-          type="text"
-          label="Name"
-          validators={[VALIDATOR_REQUIRE()]}
-          errorText="Please enter a Name."
-          onInput={inputHandler}
-        />
-        )}
-        <Input
-          id="email"
-          element="input"
-          type="email"
-          label="E-mail"
-          validators={[VALIDATOR_EMAIL()]}
-          errorText="Please enter a valid e-mail."
-          onInput={inputHandler}
-        />
-        <Input
-          id="password"
-          element="input"
-          type="password"
-          label="Password"
-          validators={[VALIDATOR_MINLENGTH(8)]}
-          errorText="Please enter a password. Min length - 8 characters."
-          onInput={inputHandler}
-        />
-        <Button
-          disabled={!formState.isValid}
-        >
-          {isLogginMode ? 'LOGIN' : 'SIGNUP'}
-        </Button>
-        <Button inverse onClick={switchLoggingMode}>SWITCH TO {isLogginMode ? 'SIGNUP' : 'LOGIN'}</Button>
-      </form>
-    </Card>
+            id="email"
+            element="input"
+            type="email"
+            label="E-mail"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Please enter a valid e-mail."
+            onInput={inputHandler}
+          />
+          <Input
+            id="password"
+            element="input"
+            type="password"
+            label="Password"
+            validators={[VALIDATOR_MINLENGTH(8)]}
+            errorText="Please enter a password. Min length - 8 characters."
+            onInput={inputHandler}
+          />
+          <Button
+            disabled={!formState.isValid}
+          >
+            {isLogginMode ? 'LOGIN' : 'SIGNUP'}
+          </Button>
+          <Button inverse onClick={switchLoggingMode}>SWITCH TO {isLogginMode ? 'SIGNUP' : 'LOGIN'}</Button>
+        </form>
+      </Card>
+    </>
   )
 };
 
